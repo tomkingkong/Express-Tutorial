11696e5ac49c839fa15c40964987f605da9736ad
Tom King <design.tomking@gmail.com>
2018-09-24 23:12:29 -0600

    Add static public folder

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.static('public'));
 app.get('/sunsets', (request, response) => {
   response.status(200).json(sunsets);
 });
