2634e943fb292cd7f4f6ec043b67beb5e12e05c6
Tom King <design.tomking@gmail.com>
2018-09-24 23:10:00 -0600

    Add sunsets

---
+++ sunsets.js
@@ -0,0 +1,9 @@
+const sunsets = `
+<img class="sunset" src="https://images.unsplash.com/photo-1505487985217-c1f95816212d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6491055a75d8ffa83dd072e10ac557a8&auto=format&fit=crop&w=800&q=60" alt="sunset" />
+<img class="sunset" src="https://images.unsplash.com/photo-1510971875972-936a57163965?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=042eb22e26bec99fcf3e17ccc67be098&auto=format&fit=crop&w=2850&q=80" alt="sunset" />
+<img class="sunset" src="https://images.unsplash.com/photo-1504217752415-b94aa513f697?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8cc71e0f5290f9e412f7367d82f1a397&auto=format&fit=crop&w=800&q=60" alt="sunset" />
+<img class="sunset" src="https://images.unsplash.com/photo-1529306591955-bcde38026933?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=13367520158f1c5ec7dc69bf9161c5a4&auto=format&fit=crop&w=800&q=60" alt="sunset" />
+<img class="sunset" src="https://images.unsplash.com/photo-1510218129079-74e00c5a90ea?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e1eaa8e8288e517b7484a017db17280d&auto=format&fit=crop&w=800&q=60" alt="sunset" />
+`
+
+module.exports = sunsets;
