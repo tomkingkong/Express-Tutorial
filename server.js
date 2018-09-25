b361d5dbceceafc3df7b6d007c28f4209bade552
Tom King <design.tomking@gmail.com>
2018-09-24 23:11:37 -0600

    Add json path and response return

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+app.get('/json', (request, response) => {
+  response.status(200).json(mockData);
+});
 app.listen(3000, () => {
   console.log('Hey man, Express is up on localhost:3000');
 });
