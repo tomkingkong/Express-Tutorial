ffe5a7f660d6c24a02668b585439a105c193fd3a
Tom King <design.tomking@gmail.com>
2018-09-24 23:09:54 -0600

    Add mockData

---
+++ mockData.js
@@ -0,0 +1,319 @@
+const mockData = [
+  {
+    "_id": "5ba9b40ad42391c93d9aae95",
+    "index": 0,
+    "guid": "ca373297-d975-4c0b-aec4-92d6d360271a",
+    "isActive": false,
+    "balance": "$2,480.53",
+    "picture": "http://placehold.it/32x32",
+    "age": 29,
+    "eyeColor": "brown",
+    "name": "Brenda Russo",
+    "gender": "female",
+    "company": "DATACATOR",
+    "email": "brendarusso@datacator.com",
+    "phone": "+1 (953) 587-2289",
+    "address": "920 Fayette Street, Woodruff, Nevada, 3498",
+    "about": "Do non pariatur laborum dolore cillum velit consequat sunt nostrud. Minim et esse Lorem deserunt in. Enim ipsum occaecat quis magna eiusmod minim ad amet consectetur nulla adipisicing sint.\r\n",
+    "registered": "2015-08-15T05:19:11 +06:00",
+    "latitude": 88.81196,
+    "longitude": 134.371282,
+    "tags": [
+      "ullamco",
+      "consectetur",
+      "adipisicing",
+      "amet",
+      "dolor",
+      "duis",
+      "nostrud"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Hunter Fuentes"
+      },
+      {
+        "id": 1,
+        "name": "Daniel Delacruz"
+      },
+      {
+        "id": 2,
+        "name": "Marisa Harris"
+      }
+    ],
+    "greeting": "Hello, Brenda Russo! You have 4 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "5ba9b40ab913151564afaf77",
+    "index": 1,
+    "guid": "df5ace4b-bef6-4df3-9382-9e334c6fbde5",
+    "isActive": false,
+    "balance": "$1,031.32",
+    "picture": "http://placehold.it/32x32",
+    "age": 37,
+    "eyeColor": "blue",
+    "name": "Wilder Norris",
+    "gender": "male",
+    "company": "NEUROCELL",
+    "email": "wildernorris@neurocell.com",
+    "phone": "+1 (944) 485-3134",
+    "address": "325 Mill Road, Chumuckla, Massachusetts, 9601",
+    "about": "Cillum consectetur excepteur qui voluptate. Elit cillum nostrud sunt laborum elit consequat velit veniam adipisicing culpa quis. Adipisicing labore magna ipsum consectetur voluptate eu ut sint consequat eu et elit sint minim.\r\n",
+    "registered": "2016-04-07T12:41:34 +06:00",
+    "latitude": 31.50679,
+    "longitude": -40.823451,
+    "tags": [
+      "incididunt",
+      "velit",
+      "voluptate",
+      "cillum",
+      "irure",
+      "ut",
+      "in"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Sampson Washington"
+      },
+      {
+        "id": 1,
+        "name": "Cannon Osborne"
+      },
+      {
+        "id": 2,
+        "name": "Zamora Holland"
+      }
+    ],
+    "greeting": "Hello, Wilder Norris! You have 4 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "5ba9b40a490a36f0577ac4d9",
+    "index": 2,
+    "guid": "db26ad00-a55c-4ece-969d-84756fb7fb37",
+    "isActive": false,
+    "balance": "$3,464.87",
+    "picture": "http://placehold.it/32x32",
+    "age": 24,
+    "eyeColor": "green",
+    "name": "Morton Nicholson",
+    "gender": "male",
+    "company": "MIXERS",
+    "email": "mortonnicholson@mixers.com",
+    "phone": "+1 (870) 524-2573",
+    "address": "107 Prince Street, Iola, Oregon, 5412",
+    "about": "Amet anim consectetur reprehenderit quis enim voluptate consectetur veniam commodo consectetur aliqua cupidatat. Culpa qui tempor nostrud magna qui labore sint fugiat non laborum amet voluptate pariatur. Sint cillum laborum sit esse. Sunt pariatur ex exercitation duis pariatur anim mollit proident. Cillum proident duis sint veniam est id qui. Lorem eu veniam ad sunt esse est nostrud nulla nisi labore cillum quis occaecat.\r\n",
+    "registered": "2014-02-01T04:56:51 +07:00",
+    "latitude": -23.00076,
+    "longitude": 21.302762,
+    "tags": [
+      "commodo",
+      "est",
+      "qui",
+      "sit",
+      "magna",
+      "reprehenderit",
+      "voluptate"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Graves Campos"
+      },
+      {
+        "id": 1,
+        "name": "Liliana Park"
+      },
+      {
+        "id": 2,
+        "name": "Gomez Foster"
+      }
+    ],
+    "greeting": "Hello, Morton Nicholson! You have 8 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "5ba9b40a14cd3997502a54bb",
+    "index": 3,
+    "guid": "2a455433-746d-4a98-b578-a64a94a4859f",
+    "isActive": true,
+    "balance": "$3,953.36",
+    "picture": "http://placehold.it/32x32",
+    "age": 40,
+    "eyeColor": "brown",
+    "name": "May Orr",
+    "gender": "female",
+    "company": "TRASOLA",
+    "email": "mayorr@trasola.com",
+    "phone": "+1 (821) 488-3339",
+    "address": "485 Bank Street, Herbster, Illinois, 5387",
+    "about": "Aliquip adipisicing in aliqua aute incididunt sunt ullamco amet cillum laborum dolor laboris nisi. Proident et dolore et et tempor mollit excepteur amet exercitation et duis sit sit culpa. Nostrud voluptate do pariatur culpa duis. Qui culpa cillum sunt elit. Ipsum mollit duis nulla consectetur cupidatat id mollit do adipisicing sit minim. Tempor amet veniam do ullamco sint proident duis occaecat ad adipisicing aute. Amet eu voluptate elit labore est do ipsum velit enim minim labore laborum officia irure.\r\n",
+    "registered": "2017-04-04T08:17:09 +06:00",
+    "latitude": 72.478218,
+    "longitude": -13.366082,
+    "tags": [
+      "ullamco",
+      "irure",
+      "elit",
+      "mollit",
+      "commodo",
+      "eu",
+      "ut"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Andrews Bernard"
+      },
+      {
+        "id": 1,
+        "name": "Melba Hardin"
+      },
+      {
+        "id": 2,
+        "name": "Nunez Rivera"
+      }
+    ],
+    "greeting": "Hello, May Orr! You have 9 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "5ba9b40ac988640bfbccc26e",
+    "index": 4,
+    "guid": "9bee6d62-c0c3-4c10-88b3-802b411f4a2d",
+    "isActive": false,
+    "balance": "$2,385.26",
+    "picture": "http://placehold.it/32x32",
+    "age": 32,
+    "eyeColor": "brown",
+    "name": "Salazar Pittman",
+    "gender": "male",
+    "company": "MULTRON",
+    "email": "salazarpittman@multron.com",
+    "phone": "+1 (917) 492-2203",
+    "address": "330 Boardwalk , Coinjock, District Of Columbia, 9251",
+    "about": "Duis aliquip esse duis sint commodo elit cupidatat proident deserunt. Adipisicing officia esse adipisicing reprehenderit laboris cupidatat eu sint ea non ea ad. Est aliqua ea magna qui nulla. Ipsum sit est veniam voluptate laborum enim dolor amet Lorem.\r\n",
+    "registered": "2017-11-24T08:35:52 +07:00",
+    "latitude": -42.476683,
+    "longitude": -0.988725,
+    "tags": [
+      "mollit",
+      "officia",
+      "non",
+      "aute",
+      "ex",
+      "ex",
+      "mollit"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Jolene Fulton"
+      },
+      {
+        "id": 1,
+        "name": "Louella Whitehead"
+      },
+      {
+        "id": 2,
+        "name": "Gordon Turner"
+      }
+    ],
+    "greeting": "Hello, Salazar Pittman! You have 7 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "5ba9b40aff083f38cf96d7fe",
+    "index": 5,
+    "guid": "132c274a-250d-4511-be98-43c346fba34e",
+    "isActive": false,
+    "balance": "$1,061.67",
+    "picture": "http://placehold.it/32x32",
+    "age": 26,
+    "eyeColor": "brown",
+    "name": "Golden Roberson",
+    "gender": "male",
+    "company": "ZIALACTIC",
+    "email": "goldenroberson@zialactic.com",
+    "phone": "+1 (875) 576-2202",
+    "address": "734 Milton Street, Maplewood, Palau, 758",
+    "about": "Ad minim veniam occaecat eu enim voluptate ex pariatur. Esse nostrud mollit enim anim anim. Id sunt esse in esse culpa ullamco qui Lorem non voluptate. Laborum do irure culpa eu sit.\r\n",
+    "registered": "2014-03-31T10:50:56 +06:00",
+    "latitude": -77.824771,
+    "longitude": 66.291705,
+    "tags": [
+      "duis",
+      "laboris",
+      "commodo",
+      "ad",
+      "ipsum",
+      "dolore",
+      "consequat"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Mclaughlin Durham"
+      },
+      {
+        "id": 1,
+        "name": "Vivian Calhoun"
+      },
+      {
+        "id": 2,
+        "name": "Samantha Sanford"
+      }
+    ],
+    "greeting": "Hello, Golden Roberson! You have 7 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "5ba9b40ac1f627d3b307128a",
+    "index": 6,
+    "guid": "9889e023-aa3c-481d-80fe-822069140f6d",
+    "isActive": true,
+    "balance": "$3,527.97",
+    "picture": "http://placehold.it/32x32",
+    "age": 35,
+    "eyeColor": "blue",
+    "name": "Bishop Vaughn",
+    "gender": "male",
+    "company": "QUONK",
+    "email": "bishopvaughn@quonk.com",
+    "phone": "+1 (942) 549-2983",
+    "address": "869 Cyrus Avenue, Wawona, North Carolina, 2320",
+    "about": "Ut qui duis quis laboris. Ullamco sint exercitation irure pariatur occaecat ex dolore sint est esse. Lorem id elit et exercitation adipisicing dolore reprehenderit ipsum aute aliqua dolor id mollit. Voluptate deserunt irure dolore aliquip. Cillum ea commodo in pariatur qui esse proident exercitation pariatur nostrud.\r\n",
+    "registered": "2014-05-05T03:10:45 +06:00",
+    "latitude": -73.789787,
+    "longitude": -142.086344,
+    "tags": [
+      "ullamco",
+      "eiusmod",
+      "laboris",
+      "nulla",
+      "ad",
+      "aute",
+      "ut"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Wilkinson Wilson"
+      },
+      {
+        "id": 1,
+        "name": "Mcbride Hinton"
+      },
+      {
+        "id": 2,
+        "name": "Maynard Beck"
+      }
+    ],
+    "greeting": "Hello, Bishop Vaughn! You have 8 unread messages.",
+    "favoriteFruit": "apple"
+  }
+]
+
+module.exports = mockData;
